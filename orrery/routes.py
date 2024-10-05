43820875396cf6b819c3e536f0981bdb3590ffbf
steeevan <48275985+steeevan@users.noreply.github.com>
2024-10-05 15:19:23 -0700

    Yanming (#1)
    * First Push
    
    * I added buttons and images to it.
    
    * Modifications
    
    ---------
    
    Co-authored-by: Yanming Hwang <plexer81@gmail.com>

---
+++ orrery/routes.py
@@ -0,0 +1,18 @@
+import requests
+from flask import Blueprint, render_template, jsonify, request
+
+class MainRoutes:
+    def __init__(self):
+        # Create a Blueprint for the main routes
+        self.blueprint = Blueprint('main',__name__)
+        self.register_routes()
+
+    def register_routes(self):
+        # define route handlers
+        @self.blueprint.route('/')
+        def index():
+            return render_template('index.html')
+      
+        @self.blueprint.route('/aboutus')
+        def aboutus():
+              return render_template('aboutUS.html')
